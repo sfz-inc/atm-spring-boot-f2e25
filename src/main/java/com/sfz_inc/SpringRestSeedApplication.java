7d52b61925ef434c677313565c18a2efece865e8
Will Gardella <WillGardella@users.noreply.github.com>
2017-07-12 10:05:03 -0700

    Add Spring Boot REST Service

---
+++ src/main/java/com/sfz_inc/SpringRestSeedApplication.java
@@ -0,0 +1,12 @@
+package com.sfz_inc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSeedApplication.class, args);
+    }
+}
